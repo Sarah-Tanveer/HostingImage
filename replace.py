4552d5478bf5ef7cd116da789ec2d34dc8d4b302
Hafsa Akbar <hafsaakbar@icloud.com>
2023-06-22 18:35:55 +0500

    move script to parent dir, create reduced_new.html in every folder

--- replace.py
+++ replace.py
@@ -1,19 +1,25 @@
 import re
+import os
 
-with open("opera_weebly.com/info/reduced.html", "r") as f:
-  html_content = f.read()
+folders = []
+path_ = os.getcwd()
+dir_list = os.listdir(path_)
+for i in dir_list:
+    path = f"{path_}/{i}"
+    if not os.path.isfile(path) and "reduced.html" in os.listdir(f"{path}/info"):
+      folders.append(i)
 
-img_src_regex = r"<img(.*?)src=\"(.*?)\""
-img_srcs = re.findall(img_src_regex, html_content)
-for img_src in img_srcs:
-  img_src = img_src[-1]
-  if "localhost" in img_src:
-      name = img_src.split("/")[-1]
-      url = f"http://github.com/Sarah-Tanveer/HostingImage/raw/main/opera_weebly.com/{name}"
-      print(url)
-      html_content = html_content.replace(img_src, url)
-with open("reduced_new.html", "w+") as f:
-  f.write(html_content)
+for folder in folders:
+  with open(f"{folder}/info/reduced.html", "r") as f:
+    html_content = f.read()
 
-  
-
+  img_src_regex = r"<img(.*?)src=\"(.*?)\""
+  img_srcs = re.findall(img_src_regex, html_content)
+  for img_src in img_srcs:
+    img_src = img_src[-1]
+    if "localhost" in img_src:
+        name = img_src.split("/")[-1]
+        url = f"http://github.com/Sarah-Tanveer/HostingImage/raw/main/{folder}/{name}"
+        html_content = html_content.replace(img_src, url)
+  with open(f"{folder}/info/reduced_new.html", "w+") as f:
+    f.write(html_content)
